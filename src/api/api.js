21629e8870cee353566c609ae2ef6fe4137e9062
he2014 <127531644@qq.com>
2018-05-12 15:53:47 +0800

    。。

--- src/api/api.js
+++ src/api/api.js
@@ -4,7 +4,7 @@ class API extends Server {
     async getHots() {
         try {
             let result = await this.axiosGet("get", "service/room/v3", { prv: "" });
-            if (result && result instanceof Object && result.code == 0) return result.dataInfo;
+            if (result && result instanceof Object && result.code === 0) return result.dataInfo;
             else this.resultCode(result.code);
         } catch (error) {
             throw error;
