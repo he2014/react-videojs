ea7b423a307280a0865018213eee893f75ff7fac
he2014 <>
2018-07-10 14:53:21 +0800

    添加 stroe 注释

--- src/store/store.js
+++ src/store/store.js
@@ -2,7 +2,9 @@ import { createStore, combineReducers, applyMiddleware } from 'redux';
 import * as chat from './chartDetail/reducer';
 import * as header from "./header/reducer"
 import thunk from 'redux-thunk';
-
+// redux-thunk中间件可以让action创建函数先不返回一个action对象，
+//而是返回一个函数，函数传递两个参数(dispatch,getState),在函数体内进行业务逻辑的封装
+//applyMiddleware方法主要是对redux的dispacth方法进行封装
 console.log(header)
 let store = createStore(
     combineReducers({ ...chat, ...header }),
