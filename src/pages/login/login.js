b46c6ee97d12eba8b023486a5326ea137a41037a
he2014 <127531644@qq.com>
2018-07-18 22:00:06 +0800

    66

--- src/pages/login/login.js
+++ src/pages/login/login.js
@@ -1,7 +1,7 @@
 import React from "react"
 
 export default (props) => {
-    return <div style={{ width: "300px", padding: "20px 0", margin: "0 auto" }} onClick={() => {
+    return <div style={{ width: "300px", padding: "20px 0", margin: "0 auto", fontSize: "50px" }} onClick={() => {
         console.log(props)
         props.history.push(props.location.state.from)
     }}> 登陆</div>
